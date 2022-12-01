a39f36e8823c2b2c6d4c9b6d30f45f531a2aa1f4
Malka Bruk <malka2039@gmail.com>
2022-12-02 00:43:11 +0200

    calculate

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/do')
+def do_some_logic():
+    x = int(request.args.get('x', ''))
+    return f'{x*x}'
